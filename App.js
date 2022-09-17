0bb13060099730560c42c0d45c69454bdd1a55b1
AndyNguyen002 <ngocdungokla@gmail.com>
2022-09-17 12:13:21 +0700

    Hoan thien bai tap

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+      
+      <View style={styles.box1}>
+        <Text style={styles.justtext}>Square 1</Text>
+      </View>
+
+      <View style={styles.box2}>
+        <Text style={styles.justtext}>Square 2</Text>
+      </View>
+
+      <View style={styles.box3}>
+        <Text style={styles.justtext}>Square 3</Text>
+      </View>
+
+      
     </View>
   );
 }
@@ -15,6 +27,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    
+  },
+  box1: {
+    backgroundColor: '#7BE0F9',
+    width: 100,
+    height:100,
     justifyContent: 'center',
+    alignItems:'center',
+    
+  },
+  box2: {
+    backgroundColor: '#4cc2c2',
+    width: 100,
+    height:100,
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+  box3: {
+    backgroundColor: '#ff637c',
+    width: 100,
+    height:100,
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+  justtext:{
+    fontSize:12
   },
 });
